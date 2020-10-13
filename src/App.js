2d9ad29fee810c2671d44ae44c890e1b06be98bc
Carl Li <leekunhui@sina.cn>
2020-10-13 22:21:40 +1300

    10 Components, props & propTypes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <h4>This is finder App</h4>
-    </div>
-  );
+class App extends Component {
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
